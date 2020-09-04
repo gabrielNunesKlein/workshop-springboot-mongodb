80a7a4c19679afddd27790782583e5ae3edb895e
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-09-04 19:17:01 -0300

    inserindo anotacao @Autowaired no atributo UserService no controller

--- src/main/java/com/gnk/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gnk/workshopmongo/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.gnk.workshopmongo.resources;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.gnk.workshopmongo.services.UserService;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
