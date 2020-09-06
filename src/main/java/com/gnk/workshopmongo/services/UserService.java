8723198d09c30ef09c92e257698d033453d2e567
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-09-05 22:22:35 -0300

    Atualização de usuário com PUT

--- src/main/java/com/gnk/workshopmongo/services/UserService.java
+++ src/main/java/com/gnk/workshopmongo/services/UserService.java
@@ -13,28 +13,42 @@ import com.gnk.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		repo.findAll();
 		repo.deleteById(id);
 	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
 	
+	
+
+	private void updateData(User newObj, User obj) {
+		newObj.setId(obj.getId());
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
